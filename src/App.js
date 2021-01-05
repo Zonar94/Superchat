8f852eac0cab6508e2701bde59dad6752867b040
zonar <424661@student.fontys.nl>
2021-01-05 23:57:54 +0100

    Added icons
    Added icons to the app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
+firebase.initializeApp({                                      
   apiKey: "AIzaSyByzYYzzbwYkIC-TrMyoFgEgBGRm4SsqNk",
   authDomain: "superchat-2c981.firebaseapp.com",
   projectId: "superchat-2c981",
@@ -18,12 +18,12 @@ firebase.initializeApp({
   measurementId: "G-T19CR0RERM"
 })
 
-const auth = firebase.auth();
+const auth = firebase.auth();      
 const firestore = firebase.firestore();
 
 function App() {
 
-   const [user] = useAuthState(auth);
+   const [user] = useAuthState(auth);            // Signed in user is an object. Signed out user is null
 
   return (
     <div className="App">
@@ -32,24 +32,24 @@ function App() {
       </header>
 
       <section>
-        {user ? <ChatRoom /> : <SignIn />}
+        {user ? <ChatRoom /> : <SignIn />}            // if user is signed in it shows the chat room, else it shows the sign in button
       </section>
     </div>
   );
 }
 
-function SignIn() {
+function SignIn() {                                        //Signs the user in with Google
    const signInWithGoogle = () => {
-     const provider = new firebase.auth.GoogleAuthProvider();
-     auth.signInWithPopup(provider);
+     const provider = new firebase.auth.GoogleAuthProvider();    
+     auth.signInWithPopup(provider);                                   // pop up window
    }
 
    return(
-     <button onClick={signInWithGoogle}>Sign in with Google</button>
+     <button onClick={signInWithGoogle}>Sign in with Google</button>    //waits for the button
    )
 }
 
-function SignOut() {
+function SignOut() {                                   // signs the user out
   return auth.currentUser && (
     
     <button onClick={() => auth.signOut()}>Sign Out</button>
@@ -60,9 +60,8 @@ function ChatRoom(){
 
   const dummy = useRef()
 
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
-
+  const messagesRef = firestore.collection('messages');     //reference from fire store collection
+  const query = messagesRef.orderBy('createdAt').limit(25); //query from the documents
   const [messages] = useCollectionData(query, {idField: 'id'});
 
   const [formValue, setFormValue] = useState('');
@@ -81,7 +80,8 @@ function ChatRoom(){
     });
 
     setFormValue('');
-
+    
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
   }
   return (
     <>
@@ -95,20 +95,20 @@ function ChatRoom(){
 
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
 
-          <button type="submit">x</button>
+          <button type="submit">🛹</button>
       </form>
     </>
   )
 }
 
-function ChatMessage(props) {
+function ChatMessage(props) {                                         
   const { text, uid, photoURL } = props.message; 
   
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   return (
    <div className={'message ${messageClass}'}>
-    <img src={'photoURL'} />
+    <img src={photoURL} />
     <p>{text}</p>
   </div>
   )
